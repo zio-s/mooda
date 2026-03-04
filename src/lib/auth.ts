c01356f63ce983ad934479ea76a3c84cfc53d41e
seming2 <popqr3@naver.com>
2026-03-05 01:18:56 +0900

    debug: auth 테스트 엔드포인트 추가 + debug 모드 활성화

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,6 +14,7 @@ const loginSchema = z.object({
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
+  debug: true,
   trustHost: true,
   adapter: PrismaAdapter(prisma),
   providers: [
